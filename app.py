7fe65947322c6b5b40a8ca64b765ac8be73cb8a7
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-03 21:13:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,30 @@ bot = Client(
     api_id = Config.API_ID,
     api_hash = Config.API_HASH
 )
-                        
+  
+    
+@bot.on_message(filters.command(['start']))
+def start(client, message):
+    text=Translation.START_TEXT,
+    quote=False,
+    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('HELP', callback_data="ihelp")],InlineKeyboardButton('GUIDE', callback_data="iguide")]]))
+    
+           
+         
+            
+@bot.on_callback_query()
+async def cb_handler(client: bot , query: CallbackQuery);
+    data = query.data
+    if data == "ihelp":
+        
+        
+    
+            
+           
+            
+            
+           
+                
+        
+    
+
