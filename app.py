7f2a1dae0cd10446d63ecb258c782e52d1a47c59
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-03 19:52:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
 from config import Config
-from helper import OpenSettings 
+
 bot = Client(
     'simp-bot',
     bot_token = Config.BOT_TOKEN,
     api_id = Config.API_ID,
     api_hash = Config.API_HASH
 )
-# let us defines some events 
-# oof the callback .
-
-                                         
-
-@bot.on_message(filters.command(['start']))
-def start(client, message):
-  r = f"`Hello {message.from_user.username} \n\nThis is a Telegram  Bot. \n\nI am still in a beta mode as I was built by my dev recently\n\n/help for more details.\n\nChannel : @Animes_Encoded`"
-  message.reply_text(
-    text=r,
-    quote=False,
-    reply_markup=InlineKeyboardMarkup(
-      [
-        [
-          InlineKeyboardButton('Source Code 🤤', url='https://t.me/shity_man')
-          ]
-        ]
-      )
-    )
-  
-@bot.on_message(filters.command(['help']))
-def help(client, message):
-                                         
-  message.reply_text("No help modules set right now !!\n\nSoon , new update will come!")
-    
-@bot.on_callback_query()
-                                         
-                                         
-                                         
-# run the bot
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-                                         
-bot.run()
+                        
