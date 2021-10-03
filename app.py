0f5c9a16de4734d220cbdd6bccb6001a4897beb4
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-03 15:46:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,28 @@ bot = Client(
     api_id = Config.API_ID,
     api_hash = Config.API_HASH
 )
+# let us defines some events 
+async def help(event):
+    await message.reply_text(
+        "The Bot isnt fully completed yet by the Owner\n\nPlease be patient until further notice\n\nJoin @Animes_Encoded"
+    )
+async def thelp(event):
+    oof = f"`Hi {message.from_user.username} \n\nThe Bot isnt fully developed by the owner.\n\nPlease be patient until further notice"
+    message.reply_text(
+        text=oof
+        quote=False,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('Back', callback
+        
+    
+    
 # let's make some simple filters...
 
 @bot.on_message(filters.command(['start']))
 def start(client, message):
-  r = f"Hi **{message.from_user.username}**\n\nThis is one simple bot\nThis is in beta now.\n/help for more details\n**Dev- @Bro_isDarkal**"
+  r = f"`Hello {message.from_user.username} \n\nThis is a Telegram  Bot. \n\nI am still in a beta mode as I was built by my dev recently\n\n/help for more details.\n\nChannel : @Animes_Encoded`"
   message.reply_text(
     text=r,
     quote=False,
