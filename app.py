105850f2f44868a2f34ed96fa7d313ce316f777a
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-04 15:39:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,26 @@ Encoder = Client(
     api_id = Config.API_ID,
     api_hash = Config.API_HASH
 )
-  
-    
+HELP_BUTTON = InlineKeyboardMarkup(
+    [[
+        InlineKeyboardButton ("♨️ HELP", callback_data="ihelp"),
+        InlineKeyboardButton ("📋 GUIDE", callback_data="iguide")
+    ]]
+)   
+
+GUIDE_BUTTON = InlineKeyboardMarkup(
+    [[
+        InlineKeyboardButton ("Back", callback_data="beck")
+    ]]
+)
 @Encoder.on_message(filters.command(['start']))
 def start(client, message):
     text=Translation.START_TEXT,
     quote=False,
-    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('♨️ HELP', callback_data="ihelp"),InlineKeyboardButton('📋 GUIDE', callback_data="iguide")]]))
-           
-  
+    reply_markup=HELP_BUTTON  
+    
+     
+      
 @Encoder.on_callback_query()
 async def cb_handler(client: Encoder , query: CallbackQuery);
     data = query.data
@@ -26,20 +37,20 @@ async def cb_handler(client: Encoder , query: CallbackQuery);
             await query.message.edit_text(
                 text=Translation.HELP_TEXT,
                 quote=False,
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('📋 GUIDE', callback_data="iguide"),InlineKeyboardButton('Back', callback_data="beck")]]))
+                reply_markup=GUIDE_BUTTON
     elif data == "iguide":
         try:
             await query.message.edit_text(
                 text=Translation.GUIDE,
                 quote=False,
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Back', callback_data="beck")]]))
+                reply_markup=GUIDE_BUTTON
     
     elif data == "beck":
         try:
             await query.message.edit_text(
                 text=Translation.START_TEXT,
                 quote=False,
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('♨️ HELP', callback_data="ihelp"),InlineKeyboardButton('📋 GUIDE', callback_data="iguide")]]))
+                reply_markup=GUIDE_BUTTON
         except:
             pass
         
