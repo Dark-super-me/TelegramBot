ccea4a314542e386e3fdf188f44121e697fc0ab6
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-05 17:17:53 +0530

    Create bash.py

---
+++ bash.py
@@ -0,0 +1,16 @@
+#
+#@bot.on_message(filters.command(['start']))
+#def start(client, message):
+  #r = f"`Hello {message.from_user.username} \n\nThis is a Telegram  Bot. \n\nI am still in a beta mode as I was built by my dev recently\n\n/help for more details.\n\nChannel : @Animes_Encoded`"
+  #message.reply_text(
+    #text=r,
+    #quote=False,
+    #reply_markup=InlineKeyboardMarkup(
+      #[
+        #[
+          #InlineKeyboardButton('Source Code 🤤', url='https://t.me/shity_man')
+          #]
+        #]
+      #)
+    #)
+
