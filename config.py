0c349cd78ab7cb9f3378453639822a470e90327e
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-05 15:50:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
 import os 
 
 class Config:
