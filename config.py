5b71a84c282d81b982668d8b286f1c785d9ec142
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-05 16:06:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,4 +11,5 @@ class Config:
   BOT_TOKEN = os.environ.get("BOT_TOKEN")
   API_ID = int(os.environ.get("API_ID"))
   API_HASH = os.environ.get("API_HASH")
- 
+except Exception as e:
+  logger.info(e)
