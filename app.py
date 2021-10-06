07f6167d947d240354569fe41d8ee09aaa0d3906
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-06 17:05:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ bbutton.append([[InlineKeyboardButton("Back", callback_data="beck")]])
 @bot.on_message(filters.command("start"))
 async def start(client,message):
   await client.send_message(
-    chat_id,
+    message.chat.id,
     text=f"Hi {message.from_user.username} \n <b> I am one simple bot  made by @Bro_isDarkal \nPls click the below buttons to get a hint about this bot !\n\n By @Animes_Encoded.<b> \n",
     reply_markup=InlineKeyboardMarkup(button))
    
@@ -37,14 +37,17 @@ async def start(client,message):
 async def callback_handlers(_, event: CallbackQuery):
     if "ihelp" in event.data:
        await event.message.edit(
+           message.chat.id,
            text="Checkout The Available Commands Here \n\n Do Follow @Animes_Encoded\n\n If You Find This Bot Usefull❤️",
            reply_markup=InlineKeyboardMarkup(bbutton))
     elif "iguide" in event.data:
        await event.message.edit(
+         message.chat.id,
          text="Right now the bot can only compress MKV formated files and the file must be Telegram Video or Telegram Document type",
          reply_markup=InlineKeyboardMarkup(bbutton))
     elif "beck" in event.data:
        await event.message.edit(
+          message.chat.id,
           text="Hi \nIam next generation video encoder bot!\n\nUpdates will come soon\n\nnMaintained by • @Animes_Encoded",
           reply_markup=InlineKeyboardMarkup(button))
         
